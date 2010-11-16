cbef31c88dea372e8118e63d017ffcaba898978e
cewing <cewing@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-11-16 20:01:28 +0000

    uncomment all licenses we've managed to set up.  Remove any we have not (in particular, we no longer provide the QPL.  Anyone have any opinions about that??
    git-svn-id: https://svn.plone.org/svn/collective/templer.core/trunk@226888 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- src/templer/core/base.py
+++ src/templer/core/base.py
@@ -15,18 +15,17 @@ from templer.core.vars import ValidationException
 
 LICENSE_CATEGORIES = {
     'EFL' : 'License :: Eiffel Forum License (EFL)',
-    # 'NPL' : 'License :: Netscape Public License (NPL)',
+    'NPL' : 'License :: Netscape Public License 1.1 (NPL)',
     'ASL' : 'License :: OSI Approved :: Apache Software License',
-    # 'BSD' : 'License :: OSI Approved :: BSD License',
-    # 'FDL' : 'License :: OSI Approved :: GNU Free Documentation License (FDL)',
+    'BSD' : 'License :: OSI Approved :: BSD License',
+    'FDL' : 'License :: OSI Approved :: GNU Free Documentation License (FDL)',
     'GPL' : 'License :: OSI Approved :: GNU General Public License (GPL)',
     'GPL3' : 'License :: OSI Approved :: GNU General Public License (GPL)',
-    # 'LGPL' : 'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-    # 'MIT' : 'License :: OSI Approved :: MIT License',
-    # 'MPL' : 'License :: OSI Approved :: Mozilla Public License 1.0 (MPL)',
-    # 'MPL11' : 'License :: OSI Approved :: Mozilla Public License 1.1 (MPL 1.1)',
-    # 'QPL' : 'License :: OSI Approved :: Qt Public License (QPL)',
-    # 'ZPL' : 'License :: OSI Approved :: Zope Public License',
+    'LGPL' : 'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+    'MIT' : 'License :: OSI Approved :: MIT License',
+    'MPL' : 'License :: OSI Approved :: Mozilla Public License 1.0 (MPL)',
+    'MPL11' : 'License :: OSI Approved :: Mozilla Public License 1.1 (MPL 1.1)',
+    'ZPL' : 'License :: OSI Approved :: Zope Public License',
     }
 
 
