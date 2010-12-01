e209a41a616efecd0a6bc4d7cd28198a9b1ac16c
cewing <cewing@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-12-01 18:18:44 +0000

    tests for license structures. fix error in license templates exposed by tests :)
    git-svn-id: https://svn.plone.org/svn/collective/templer.core/trunk@227964 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- src/templer/core/tests/test_licenses.py
+++ src/templer/core/tests/test_licenses.py
@@ -9,6 +9,21 @@ from paste.script.command import get_commands
 from templer.core.base import BaseTemplate
 from templer.core.base import LICENSE_CATEGORIES
 
+LICENSE_EXPECTATIONS = {
+    'ASL': ['NOTICE.txt', 'LICENSE.ASL'],
+    'BSD': ['LICENSE.txt',],
+    'EFL': ['LICENSE.txt', 'LICENSE.EFL'],
+    'FDL': ['LICENSE.txt', 'LICENSE.GFDL'],
+    'GPL': ['LICENSE.txt', 'LICENSE.GPL'],
+    'GPL3': ['LICENSE.txt', 'LICENSE.GPL'],
+    'LGPL': ['LICENSE.txt', 'LICENSE.GPL', 'LICENSE.LGPL'],
+    'MIT': ['LICENSE.txt',],
+    'MPL': ['LICENSE.txt', 'LICENSE.MPL'],
+    'MPL11': ['LICENSE.txt', 'LICENSE.MPL'],
+    'NPL': ['LICENSE.txt', 'LICENSE.NPL'],
+    'ZPL': ['LICENSE.txt', 'LICENSE.ZPL'],
+}
+
 class TestLicenses(unittest.TestCase):
     """ verify that all licenses are registered, findable and output correctly 
     """
@@ -43,32 +58,43 @@ class TestLicenses(unittest.TestCase):
         for license in selectable_licenses:
             self.failUnless(license in all_structures)
     
-    def test_gpl_structure(self):
-        """ verify that the gpl2 license writes correctly, given appropriate values
-            
-            the base template requires no structures, so the only one we should
-            get below is the license structure
+    def test_license_structure(self):
+        """ verify that all license structures are well formed
         """
         this_year = datetime.date.today().year
-        try:
-            my_license = self.template.get_structures(self.license_vars)[0]
-        except IndexError:
-            self.fail('unable to find GPL license structure')
+        for this_license in LICENSE_EXPECTATIONS.keys():
+            self.license_vars['license_name'] = this_license
+            try:
+                # base template requires no structures, we should only get
+                # license structure
+                my_license = self.template.get_structures(self.license_vars)[0]
+            except IndexError:
+                self.fail('unable to find %s license structure' % this_license)
         
-        my_license().write_files(self.command, self.tempdir, self.license_vars);
-        import pdb; pdb.set_trace( )
+            my_license().write_files(self.command, 
+                                     self.tempdir, 
+                                     self.license_vars);
         
-        top = os.listdir(self.tempdir)
-        self.failUnless('docs' in top, 'failed to write the docs directory')
-        
-        docs = os.listdir(os.path.join(self.tempdir, 'docs'))
-        self.failUnless('LICENSE.GPL' in docs, 'GPL License failed to write')
-        self.failUnless('LICENSE.txt' in docs, 'license boilerplate failed to write')
-        
-        bp = open(os.path.join(self.tempdir, 'docs', 'LICENSE.txt'), 'r').read()
-        self.failUnless(self.license_vars['author'] in bp, 'Author not in license boilerplate')
-        self.failUnless(self.license_vars['project'] in bp, 'Project name not in license boilerplate')
-        self.failUnless(str(this_year) in bp, 'Current year not in license boilerplate')
+            top = os.listdir(self.tempdir)
+            self.failUnless('docs' in top, 'failed to write the docs directory')
+            expected = LICENSE_EXPECTATIONS[this_license]
+            docs = os.listdir(os.path.join(self.tempdir, 'docs'))
+            for filename in expected:
+                self.failUnless(filename in docs, 
+                                '%s not found in docs dir' % filename)
+            
+            bpfh = open(os.path.join(self.tempdir, 'docs', expected[0]), 'r')
+            bp = bpfh.read()
+            self.failUnless(self.license_vars['author'] in bp, 
+                            'Author not in license boilerplate')
+            self.failUnless(self.license_vars['project'] in bp, 
+                            'Project name not in license boilerplate')
+            self.failUnless(str(this_year) in bp, 
+                            'Current year not in license boilerplate')
+            # clean up
+            bpfh.close()
+            shutil.rmtree(self.tempdir, ignore_errors=True)
+            self.tempdir = tempfile.mkdtemp()
 
 def test_suite():
     suite = unittest.TestSuite([
