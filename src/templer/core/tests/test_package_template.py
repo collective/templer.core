68d09b671710cf711192a35d433d2f8882784bbf
cewing <cris@crisewing.com>
2012-10-31 23:27:01 -0700

    * fix location management problem in tests that causes problems when running all templer tests in series.  * pep8

--- src/templer/core/tests/test_package_template.py
+++ src/templer/core/tests/test_package_template.py
@@ -1,20 +1,14 @@
-
 import os
 import tempfile
 import unittest2 as unittest
 import pkg_resources
+import shutil
 
 from paste.script.command import get_commands
 
 from templer.core.base import BaseTemplate
-from templer.core.base import get_var
-from templer.core.vars import var
-from templer.core.vars import BooleanVar
-from templer.core.vars import StringVar
-from templer.core.vars import TextVar
 from templer.core.vars import DottedVar
 from templer.core.vars import EXPERT
-from templer.core.vars import EASY
 
 from templer.core.package_template import PackageTemplate
 
@@ -30,33 +24,29 @@ class TestPackageTemplate(unittest.TestCase):
         """ set up some basics for the coming tests
         """
         self.vars = [
-#            var('basic_var', 'This is a basic variable',
-#                title="Basic Title", default="foo",
-#                modes=(EXPERT, EASY)),
-#            BooleanVar('bool_var', 'This is a boolean variable',
-#                       title="Boolean Title", default=False, page='Main',
-##                       modes=(EASY)),
             DottedVar('egg', 'This is a string variable',
                       title="String Title", default="string", page='Carl',
-                      modes=(EXPERT)),]
-##            TextVar('txt_var', 'This is a text variable', page='Martin',
-#                    title="Text Title", default="text",
-#                    modes=()),
-#            DottedVar('dot_var', 'This is a dotted variable',
-#                      title="Dotted Title", default="dotted.variable")]
+                      modes=(EXPERT)), ]
         self.template = BaseTemplate('my_name')
         create = get_commands()['create'].load()
         command = create('create')
         command.parse_args(['-t', 'package'])
         command.interactive = False
         self.command = command
+        # create a temp directory and move there, remember where we came from
         self.orig_dir = os.getcwd()
         self.temp_dir = tempfile.mkdtemp()
         cd(self.temp_dir)
 
+    def tearDown(self):
+        # after tests are done, return to old working dir and remove the temp
+        cd(self.orig_dir)
+        shutil.rmtree(self.temp_dir, ignore_errors=True)
+        self.temp_dir = None
+
     def test_entrypoint(self):
         eps = self._structure_entry_points = list(
-                pkg_resources.iter_entry_points('paste.paster_create_template'))
+            pkg_resources.iter_entry_points('paste.paster_create_template'))
         package = None
         for ep in eps:
             if ep.name == 'package':
@@ -66,20 +56,22 @@ class TestPackageTemplate(unittest.TestCase):
     def test_template_pre_package(self):
         """Make sure no namespace is inserted"""
         new_template = PackageTemplate('joe')
-        vars = {'egg' : 'exampleproject'}
+        vars = {'egg': 'exampleproject'}
         new_template.pre(self.command, self.temp_dir, vars)
         self.failUnless(vars['namespace'] == "")
 
     def test_template_pre_dot_package(self):
         """Make sure the namespace line is inserted"""
         new_template = PackageTemplate('joe')
-        vars = {'egg' : 'example.project'}
+        vars = {'egg': 'example.project'}
         new_template.pre(self.command, self.temp_dir, vars)
-        self.failUnless(vars['namespace'] == "\n      namespace_packages=['example'],", vars['namespace'])
+        expected = "\n      namespace_packages=['example'],"
+        self.failUnless(vars['namespace'] == expected, vars['namespace'])
 
     def test_template_pre_dotdot_package(self):
         """Make sure the namespace line is inserted"""
         new_template = PackageTemplate('joe')
-        vars = {'egg' : 'example.dotdot.project'}
+        vars = {'egg': 'example.dotdot.project'}
         new_template.pre(self.command, self.temp_dir, vars)
-        self.failUnless(vars['namespace'] == "\n      namespace_packages=['example', 'example.dotdot'],", vars['namespace'])
+        expected = "\n      namespace_packages=['example', 'example.dotdot'],"
+        self.failUnless(vars['namespace'] == expected, vars['namespace'])
